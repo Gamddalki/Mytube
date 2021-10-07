60011f3e5452f02ec722d0bcf67c8baf60dd7c0a
Gamddalki <hannahk01@naver.com>
2021-10-07 21:38:23 +0900

    Design and Create routers

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Mytube
+
+/ -> Home
+/join
+/login
+/search
+
+/users/profile
+/users/delete
+
+/videos/watch
+/videos/edit
+/videos/delete
+
+/videos/comments
+/videos/comments/edit
+/videos/comments/delete
--- src/server.js
+++ src/server.js
@@ -4,6 +4,30 @@ import morgan from "morgan";
 const PORT = 6104;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const usersRouter = express.Router();
+
+const handleUsersEdit = (req, res) => res.send("Users Edit");
+
+usersRouter.get("/edit", handleUsersEdit);
+
+const videosRouter = express.Router();
+
+const handleVideosWatch = (req, res) => res.send("Videos Watch");
+
+videosRouter.get("/watch", handleVideosWatch);
+
+app.use("/", globalRouter);
+app.use("/users", usersRouter);
+app.use("/videos", videosRouter);
 
 const handlelistening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 😆`);
