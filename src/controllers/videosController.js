86d6e626eb1ed84e233babc2feb999ae3074d33f
Gamddalki <hannahk01@naver.com>
2021-10-11 17:03:01 +0900

    Create DB array

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,4 +1,32 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+const videos = [
+  {
+    title: "First Video",
+    likes: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    likes: 10,
+    comments: 4,
+    createdAt: "49 minutes ago",
+    views: 67,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    likes: 456,
+    comments: 57,
+    createdAt: "6 hours ago",
+    views: 789,
+    id: 3,
+  },
+];
+
+export const home = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) => res.send("Videos Search");
 export const watch = (req, res) => res.send("Videos Watch");
