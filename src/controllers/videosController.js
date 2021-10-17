937265a87ac0a6429172a2d7cca979f8e2a606d8
Gamddalki <hannahk01@naver.com>
2021-10-17 23:52:54 +0900

    Search video

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,12 +1,21 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ likes: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const search = (req, res) => {
-  res.send("Videos Search");
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
